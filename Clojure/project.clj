13c6beee122464c1c69529f6c4be518d57868f9e
Mike Fikes <mike@fikesfarm.com>
2015-02-03 20:15:48 -0500

    Update URL in project.cl

--- Clojure/project.clj
+++ Clojure/project.clj
@@ -1,6 +1,6 @@
 (defproject ambly "0.1.0-SNAPSHOT"
   :description "ClojureScript REPL into embedded JavaScriptCore."
-  :url "https://github.com/mfikes/ambly"
+  :url "https://github.com/omcljs/ambly"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
