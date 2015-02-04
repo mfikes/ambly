16f36d64ef4fde7a568bc1617e583c95775e32e0
Mike Fikes <mike@fikesfarm.com>
2015-02-03 22:24:32 -0500

    Eliminate a bunch of unused code In the demo iOS "host app" there are a lot of empty override methods that are not needed.

--- ObjectiveC/Ambly Demo/Ambly Demo/ViewController.m
+++ ObjectiveC/Ambly Demo/Ambly Demo/ViewController.m
@@ -6,14 +6,4 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 @end
