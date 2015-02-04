be6a3b20fb86d8f2289edc21f77786b736e40f41
Mike Fikes <mike@fikesfarm.com>
2015-02-03 20:14:05 -0500

    Remove references to me Code is now part of omcljs group

--- ObjectiveC/Ambly Demo/Ambly Demo/main.m
+++ ObjectiveC/Ambly Demo/Ambly Demo/main.m
@@ -1,11 +1,3 @@
-//
-//  main.m
-//  Ambly Demo
-//
-//  Created by Mike Fikes on 1/9/15.
-//  Copyright (c) 2015 Mike Fikes. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
