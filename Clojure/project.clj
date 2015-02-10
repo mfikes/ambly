691ee801629c21c958f069cda94934a64283b8c2
Mike Fikes <mike@fikesfarm.com>
2015-02-09 21:10:07 -0500

    Update to ClojureScript 0.0-2814

--- Clojure/project.clj
+++ Clojure/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2760"]]
+                 [org.clojure/clojurescript "0.0-2814"]]
   :source-paths ["src"]
   :compiler {
      :output-dir "out"
