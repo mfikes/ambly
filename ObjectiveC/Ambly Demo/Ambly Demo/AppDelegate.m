9c4f2f16898329e2494814a0a9ecd7e22bd13528
Mike Fikes <mike@fikesfarm.com>
2015-02-13 12:59:28 -0500

    Hook WebDav server directly into Ambly Demo

--- ObjectiveC/Ambly Demo/Ambly Demo/AppDelegate.m
+++ ObjectiveC/Ambly Demo/Ambly Demo/AppDelegate.m
@@ -2,20 +2,31 @@
 
 #import "ABYServer.h"
 #import "ABYContextManager.h"
+#import "GCDWebDAVServer.h"
 
 @interface AppDelegate ()
 
 @property (strong, nonatomic) ABYContextManager* contextManager;
-@property (strong, nonatomic) ABYServer* server;
+@property (strong, nonatomic) ABYServer* replServer;
+@property (strong, nonatomic) GCDWebDAVServer* davServer;
 
 @end
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    // Start up the REPL server
     self.contextManager = [[ABYContextManager alloc] init];
-    self.server = [[ABYServer alloc] init];
-    [self.server startListening:50505 forContext:self.contextManager.context];
+    self.replServer = [[ABYServer alloc] init];
+    [self.replServer startListening:50505 forContext:self.contextManager.context];
+    
+    // Start up the WebDAV server
+    NSString* documentsPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    self.davServer = [[GCDWebDAVServer alloc] initWithUploadDirectory:documentsPath];
+    [self.davServer start];
+    NSLog(@"Visit %@ in your WebDAV client", self.davServer.serverURL);
+    
     return YES;
 }
 
