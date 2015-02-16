4eb0561ba12082c2badcfe4092838857d9ba6b7b
Mike Fikes <mike@fikesfarm.com>
2015-02-16 16:35:35 -0500

    Missed file on last commit.

--- Clojure/project.clj
+++ Clojure/project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2850"]]
+                 [org.clojure/clojurescript "0.0-2850"]
+                 [javax.jmdns/jmdns "3.4.1"]]
   :source-paths ["src"]
   :compiler {
      :output-dir "out"
