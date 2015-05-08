69131a0de84937bb5803c52ac1535c38f87d62d2
Mike Fikes <mike@fikesfarm.com>
2015-05-08 09:39:27 -0400

    Latest ClojureScript

--- Clojure/project.clj
+++ Clojure/project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/omcljs/ambly"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
-                 [org.clojure/clojurescript "0.0-3211"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+                 [org.clojure/clojurescript "0.0-3246"]
                  [com.github.rickyclarkson/jmdns "3.4.2-r353-1"]]
   :source-paths ["src"]
   :main ambly.repl.jsc)
