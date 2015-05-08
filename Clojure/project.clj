ca7960cf71e1aff336126af3600632b544c43da9
Mike Fikes <mike@fikesfarm.com>
2015-05-08 15:06:58 -0400

    Update to ClojureScript 0.0-3255

--- Clojure/project.clj
+++ Clojure/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                 [org.clojure/clojurescript "0.0-3246"]
+                 [org.clojure/clojurescript "0.0-3255"]
                  [com.github.rickyclarkson/jmdns "3.4.2-r353-1"]]
   :source-paths ["src"]
   :main ambly.repl.jsc)
