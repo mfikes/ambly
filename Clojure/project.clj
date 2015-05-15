0751fcedba7a47e08c4b09fc19df7a9b1d142422
Mike Fikes <mike@fikesfarm.com>
2015-05-15 09:15:37 -0400

    Update Clojure and ClojureScript deps
    1.7.0-beta3
    0.0-3269

--- Clojure/project.clj
+++ Clojure/project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/omcljs/ambly"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
-                 [org.clojure/clojurescript "0.0-3255"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
+                 [org.clojure/clojurescript "0.0-3269"]
                  [com.github.rickyclarkson/jmdns "3.4.2-r353-1"]])
