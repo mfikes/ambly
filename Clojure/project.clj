a248d3c84bc9fb428606973e3dc72697047c2f11
Mike Fikes <mike@fikesfarm.com>
2015-05-29 18:06:41 -0400

    0.5.0

--- Clojure/project.clj
+++ Clojure/project.clj
@@ -1,4 +1,4 @@
-(defproject org.omcljs/ambly "0.4.0"
+(defproject org.omcljs/ambly "0.5.0"
   :description "ClojureScript REPL into embedded JavaScriptCore."
   :url "https://github.com/omcljs/ambly"
   :license {:name "Eclipse Public License"
