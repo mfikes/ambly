821cdd28458e5165a86b26da1c860d535cd1a788
Mike Fikes <mike@fikesfarm.com>
2018-03-25 15:23:43 -0400

    1.3.0

--- Clojure/project.clj
+++ Clojure/project.clj
@@ -1,8 +1,8 @@
-(defproject ambly "1.2.0"
+(defproject ambly "1.3.0"
   :description "ClojureScript REPL into embedded JavaScriptCore."
   :url "https://github.com/mfikes/ambly"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.9.946" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.238" :scope "provided"]
                  [com.github.rickyclarkson/jmdns "3.4.2-r353-1"]])
