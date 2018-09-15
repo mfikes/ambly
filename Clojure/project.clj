7431f796ff25e9556ab4250d0f60ad818155ea88
Mike Fikes <mike@fikesfarm.com>
2018-09-15 08:46:32 -0400

    Update to ClojureScript 1.10.339

--- Clojure/project.clj
+++ Clojure/project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.10.238" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.339" :scope "provided"]
                  [com.github.rickyclarkson/jmdns "3.4.2-r353-1"]])
